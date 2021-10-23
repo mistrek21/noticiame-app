2ebbd14a61941efbc9b2bea027bb267b20c02eed
mata <mata@matas-MacBook-Air.local>
2021-10-23 13:26:59 +0200

    firebase minimal config with google auth and downgradae to firebase8

--- pages/enter.js
+++ pages/enter.js
@@ -1,9 +1,46 @@
-function EnterPage() {
+import { auth, googleAuthProvider } from '../lib/firebase'
+
+function Enter() {
+    const user = null
+    const username = null
+
+    // 1. User signed out <SignInButton />
+    // 2. User signed in, but missing username <UsernameForm />
+    // 3. User signed in, has useername <SignOutButton />
+
     return (
-        <div>
-            <h1>Log In</h1>
-        </div>
+        <main>
+            {user ?
+                !username ? <UsernameForm /> : <SignInButton />
+                :
+                <SignInButton />
+            }
+        </main>
     )
 }
 
-export default EnterPage
+export default Enter
+
+// Sign in with Google button
+function SignInButton() {
+    const signInWithhGoogle = async () => {
+        await auth.signInWithPopup(googleAuthProvider)
+    }
+
+    return (
+        <button className="btn-google" onClick={signInWithhGoogle}>
+            <img src={'/google.png'} /> Sign in with Google
+        </button>
+    )
+
+}
+
+// Sign out button
+function SignOutButton() {
+    return <button onLcick={() => auth.signOut()}>Sign Out</button>
+}
+
+// Username button
+function UsernameForm() {
+
+}
